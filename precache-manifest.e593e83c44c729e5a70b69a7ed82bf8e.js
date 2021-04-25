78815cbc56e0f740459b1bcdc6b9e2dc568e3a42
Sahaj-Arya <sahajarya9@gmail.com>
2021-04-25 14:49:20 +0530

    Updates

---
+++ precache-manifest.e593e83c44c729e5a70b69a7ed82bf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdd40a419c35259b78bfeefd43a0fd4",
+    "url": "/ZEAL-MEET/index.html"
+  },
+  {
+    "revision": "b6847631fdaf76d4a952",
+    "url": "/ZEAL-MEET/static/css/2.7679ac31.chunk.css"
+  },
+  {
+    "revision": "c89bcbd71b3c60268d9a",
+    "url": "/ZEAL-MEET/static/css/main.81fd7833.chunk.css"
+  },
+  {
+    "revision": "b6847631fdaf76d4a952",
+    "url": "/ZEAL-MEET/static/js/2.4e964c00.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/ZEAL-MEET/static/js/2.4e964c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89bcbd71b3c60268d9a",
+    "url": "/ZEAL-MEET/static/js/main.e6f2fdf0.chunk.js"
+  },
+  {
+    "revision": "847dab06287deeed5dc6",
+    "url": "/ZEAL-MEET/static/js/runtime-main.33b8c2f1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ZEAL-MEET/precache-manifest.e593e83c44c729e5a70b69a7ed82bf8e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZEAL-MEET/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
